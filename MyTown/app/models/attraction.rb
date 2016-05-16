ad6ce37fa65cda265600096dad039f3331c560fa
Ally Sorge <asorge@andrew.cmu.edu>
2016-05-16 09:28:32 -0400

    fixed attraction coordinate bug

--- MyTown/app/models/attraction.rb
+++ MyTown/app/models/attraction.rb
@@ -5,4 +5,17 @@ class Attraction < ActiveRecord::Base
 	# Scopes
 	scope :alphabetical, -> { order('name') }
 	
+	
+	before_validation :find_attraction_coordinates, :if => :street_changed?
+
+	def find_attraction_coordinates
+		 coord = Geocoder.coordinates("#{street}, #{town.name}, #{town.state}")
+		 if coord
+	      self.latitude = coord[0]
+	      self.longitude = coord[1]
+	    else 
+	      errors.add(:base, "Error with geocoding")
+	    end
+	    coord
+	end
 end
--- MyTown/app/models/town.rb
+++ MyTown/app/models/town.rb
@@ -20,18 +20,7 @@ class Town < ActiveRecord::Base
 	    coord
 	end
 
-	def find_attraction_coordinates
-		 coord = Geocoder.coordinates("#{street}, #{town.name}, #{town.state}")
-		 if coord
-	      self.latitude = coord[0]
-	      self.longitude = coord[1]
-	    else 
-	      errors.add(:base, "Error with geocoding")
-	    end
-	    coord
-	end
-
 	private
 	before_validation :find_town_coordinates
-	before_validation :find_attraction_coordinates, :if => :street_changed?
+	
 end
